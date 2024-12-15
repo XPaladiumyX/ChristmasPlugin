e4915bdc98d7d46791159e17c73ed3f0c71d1b89
XPaladiumyX <allegra.yann@gmail.com>
2024-12-15 18:14:42 +0100

    first commit

---
+++ src/main/java/skyxnetwork/christmasPlugin/ChristmasPlugin.java
@@ -0,0 +1,17 @@
+package skyxnetwork.christmasPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChristmasPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
